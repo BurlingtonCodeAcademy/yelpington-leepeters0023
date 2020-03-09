de9e7d42833989e863ac5c93dea87947ff33af90
Lee Peters <lpeters0023@gmail.com>
2020-03-09 11:25:47 -0400

    bob's feedback

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
 const PORT = process.env.PORT || 8000;
 
 app.use(express.static('public'));
@@ -8,4 +9,9 @@ app.get('/post/:id', (req, res) => {
   res.sendFile(path.resolve('public/post.html'))
 })
 
+app.post('public/post/:id', async (req, res) => {
+ let restDoc = fs.readFileSync(`./locations.json/${req.params.id}.json`) // this reads JSON file and stores it in that variable. We always want to await readFile because reading files takes time and if we keep cycling through we'll be given a 'promise pending' object, which we don't want. What does 'params' do? and why .json on the end? 
+  restDoc.comments.push(req.body.comment// <- name of input field)
+  await fs.writeFile()
+})
 app.listen(PORT, () => {console.log('yup')}) 
--- public/post.js
+++ public/post.js
@@ -7,11 +7,42 @@ let hours;
 let notes;
 let restaurant;
 let coords;
-let post;
+let post; // do dom queries here 
 let count = 0;
-let name = document.location.hash.slice(1) 
+let name = document.location.hash.slice(1) // be aware that this returns an array // this is actually id and was correct the first time, what has been clicked 
+let displayName = document.getElementById('leftNav') // do dom queries for each element 
 
-getPost();
+// let query = window.location.search // gives you the question mark and anything after it 
+
+let id = query.split('?')[1] // splits at the ? and the 1 means we take everything after the ? 
+
+fetch(`/locations.json`) 
+  .then(res => res.json()) // this is called an implicit return, if we're not passing anything we don't need to curly braces, I think. Doesn't actually matter. 
+  .then((jsonRes) => {
+    console.log(jsonRes) // json res is an array
+    displayName.innerText = jsonRes.name
+    for (post of jsonRes) {
+      // ad if else if post id === query 
+      name = post.name;
+      address = post.address;
+      phone = post.phone;
+      website = post.website;
+      hours = post.hours;
+      notes = post.notes;
+      coords = JSON.parse(post.coords);
+      L.marker(coords).addTo(mymap)
+      name[count].innerHTML = name
+      count++
+      }
+      return jsonObj;
+    })
+}
+
+    // fun fact: we can do fetches inside of fetches
+    //fetch(`https://nominatim...${jsonRes.address}`)
+      //.then(res => res.json()) 
+  //})
+/*getPost();
 async function getPost() {
   post = await fetch(`locations.json`${:name})
     .then((response) => {
@@ -33,5 +64,6 @@ async function getPost() {
       return jsonObj;
     })
 }
+*/ 
 
-
+// single page web app 
