9ead0fb0ff291d6faa87bea43b454e9ad6dcca29
Lee Peters <lpeters0023@gmail.com>
2020-03-23 19:39:21 -0400

    found the error in app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/post/:id', (req, res) => {
 
 app.post('public/post/:id', async (req, res) => {
  let restDoc = fs.readFileSync(`./locations.json/${req.params.id}.json`) // this reads JSON file and stores it in that variable. We always want to await readFile because reading files takes time and if we keep cycling through we'll be given a 'promise pending' object, which we don't want. What does 'params' do? and why .json on the end? 
-  restDoc.comments.push(req.body.comment// <- name of input field)
+  restDoc.comments.push(req.body.comment)// <- name of input field)
   await fs.writeFile()
 })
 app.listen(PORT, () => {console.log('yup')}) 
